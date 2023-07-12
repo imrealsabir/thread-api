65cc9081a94f6e23cbbd3cadc87191db3e2e2a7e
imrealsabir <133469818+imrealsabir@users.noreply.github.com>
2023-07-12 09:25:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// Enable CORS for all routes
-app.use(cors());
+// Enable CORS for all origins
+const corsOptions = {
+  origin: '*', // Allow requests from any origin
+};
 
-app.get("/" , async ( req , res) => {
-    res.send("welcome to thread api");
-})
+app.use(cors(corsOptions));
+
+app.get("/", async (req, res) => {
+  res.send("Welcome to the thread API");
+});
 
 app.get('/download', async (req, res) => {
   try {
-    const { link } = req.query
+    const { link } = req.query;
     const apiUrl = `https://api.threadsdownloader.io/load?url=${link}`;
     const response = await axios.get(apiUrl);
     const videoUrl = response.data.media[0].url;
